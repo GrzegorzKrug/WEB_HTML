e59e4d030d4e179345a284ffed25c0d4866b11dc
GrzegorzKrug <grzesiekk321@gmail.com>
2019-05-27 02:12:37 +0200

    Revert "Update SteamMarketAPI.py"
    This reverts commit 9d294250a8af1aabc9d09f13ba79efd0590dee4c.

--- SteamMarketAPI.py
+++ SteamMarketAPI.py
@@ -1,23 +1,5 @@
 import numpy as np
-#import steam
-import shutil
-import os
-from PIL import Image
+import steam
 import json
 import requests
 
-cat_link = r'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.wisegeek.com%2Fyoung-calico-cat.jpg&f=1'
-
-cat = requests.get(cat_link, stream=True)
-
-""" Saving picture on disk """
-# picture = open('cat.png', 'wb')
-# for chunk in cat:
-#     picture.write(chunk)
-# picture.close()
-
-# cat.raw.decode_content = True
-# cat.seek(0)  # Not working
-image = Image.open(cat.raw)
-image.show()
-# os.remove('cat.png')
