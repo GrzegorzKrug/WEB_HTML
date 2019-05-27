62be43079b430efbb8af560c13632316a810cdf0
GrzegorzKrug <grzesiekk321@gmail.com>
2019-05-27 19:49:57 +0200

    Key

--- SteamMarketAPI.py
+++ SteamMarketAPI.py
@@ -19,9 +19,17 @@ def grab_image(url):
     else:
         raise BaseException
 
-""" Saving picture on disk. Loop breaks showing afterwads"""
+def read_key():
+    ''' Reading from text file, put key only!'''
+    file = open('steam_key.txt')
+    steam_key =file.read().split()[0]  # Spliting to avoid new lines in text file
+    return steam_key
 
+steam_key = read_key()
+
+''' Grabbing image'''
 cat_img = grab_image(cat_link)
+''' Saving File'''
 file_pic = open('cat.png', 'wb')
 cat_img.save(file_pic)
 file_pic.close()
@@ -29,10 +37,10 @@ file_pic.close()
 plt.imshow(cat_img)  # Ploting
 # plt.show()
 
-
+''' Grabbing Text '''
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 res.raise_for_status()
 playFile = open('RomeoAndJuliet.txt', 'wb')
-for chunk in res.iter_content(100000):
+for chunk in res:
     playFile.write(chunk)
 playFile.close()
