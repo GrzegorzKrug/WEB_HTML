7e6dd945a647b28033d4830c6db3082f5bb8c499
GrzegorzKrug <grzesiekk321@gmail.com>
2019-06-10 13:44:38 +0200

    Request as generator

--- InternetSearch.py
+++ InternetSearch.py
@@ -1,16 +1,16 @@
-from time import time
-now = time()
+# from time import time
+# now = time()
 
 import numpy as np
 # import shutil
 # import os
-# import re
+import re
 # import json
 import requests
 import bs4
 # from SteamMarketAPI import grab_image  # why it imports so long?
-after = time()-now
-print('Import time = ', after)
+# after = time()-now
+# print('Import time = ', after)
 
 
 class FindNews():
@@ -18,13 +18,12 @@ class FindNews():
         self.duckduck_search = 'https://duckduckgo.com/?q='
 
     def get_items_by_key(self, soup, excact_len=False, *args, **keys):
-        # Grabing items from google
+        # Grabing items by keys
         #
+        # returns [items^n, href_link]
         key_n = len(keys)
-        # print('args:', args)
-        # print('keys: ', keys)
         this_keys = [key.split() for key in keys.values()]
-        print(this_keys)
+        # print(this_keys)
         results = soup.find_all('a')
 
         items_out = []
@@ -34,18 +33,16 @@ class FindNews():
             for child in element.children:
                 try:
                     url = element['href']
-                    # print(str(url))
                 except KeyError:
                     url = 'Not Found in element'
-                # except TypeError:
-                #     pass
+
                 if type(child) == bs4.element.Tag:
                     try:
-                        abc = [child.text for key in this_keys if child['class'] == key]
-                        # if child['class'] == google_title.split() or child['class'] == google_adres.split():
-                        if abc != []:
-                            my_tuple.append(abc)
-                        # print(my_tuple)
+                        # abc = [child.text for key in this_keys if child['class'] == key]
+                        for key in this_keys:
+                            if child['class'] == key:
+                                my_tuple.append(child.text)
+
                     except KeyError:
                         pass
                     pass
@@ -56,49 +53,65 @@ class FindNews():
                 items_out.append(tuple(my_tuple))
         return items_out
 
-    def search_in_google(self, soup):
+    def search_in_google_html(self, soup):
 
-        # out = []
+
         google_title = 'BNeawe vvjwJb AP7Wnd'  # Google Title
-        google_adres = 'BNeawe UPmit AP7Wnd'  # Google URL
-        gazetapl = 'iUh30'
+        # google_adres = 'BNeawe UPmit AP7Wnd'  # Google URL
 
         items = self.get_items_by_key(soup, **{'google_title':google_title})
 
         return items
 
-    def search_web(self, query):
-        url = self.duckduck_search + query
-        url = 'https://duckduckgo.com/?q=usa&t=ffab&atb=v131-1&ia=news'
-        url = 'https://www.google.com/search?source=hp&ei=Y3z9XKLfM5GOrwTjjK_wCA&q=wiadomo%C5%9Bci&oq=wiad&gs_l=psy-ab.3.0.0i67j0i131i67j0i131l5j0l3.197.1046..1492...0.0..0.144.571.0j5......0....1..gws-wiz.....0..35i39j35i39i19.r1K-4RCuSk8'
+    def search_web(self, urls):
 
-        print('Requesting: {0}'.format(url))
-        req = requests.get(url, True)
-        req.raise_for_status()
-        req.raw.decode_content = True
-        soup = bs4.BeautifulSoup(req.text, "html.parser")  # as ebook says r.text !
-        return soup
+        if type(urls) is str:
+            urls = [urls]
+
+        for url in urls:
+            print('Requesting: {0}'.format(url))
+            req = requests.get(url, True)
+            req.raise_for_status()
+            req.raw.decode_content = True
+            soup = bs4.BeautifulSoup(req.text, "html.parser")  # as ebook says r.text !
+            yield soup
 
     def start_searching(self):
-        print('Searching google: wiadomosci')
-        yield True
-
-        soup = self.search_web('None')
+        #0 Grabing Google search results
+        urls = 'https://www.google.com/search?source=hp&ei=Y3z9XKLfM5GOrwTjjK_wCA&q=wiadomo%C5%9Bci&oq=wiad&gs_l' \
+               '=psy-ab.3.0.0i67j0i131i67j0i131l5j0l3.197.1046..1492...0.0..0.144.571.0j5......0....1..gws-wiz.....0..' \
+               '35i39j35i39i19.r1K-4RCuSk8'
+        soup_list = [*self.search_web(urls)]
         print('Got soup!')
         yield True
 
-        result = self.search_in_google(soup)
+        #1 Filtering First Soup
+        result = self.search_in_google_html(soup_list[0])
         print('Got links!')
-        print(result)
-
         yield
 
+        #2
+        while True:
+            print('Select web to grab news')
+            for i, item in enumerate(result):
+                print(' {0}#'.format(i).ljust(5), item[0])
+            choice = input("Give separate numbers or '*' to read all.\n")
 
-# link_market = 'https://steamcommunity.com/market/search?q=&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&appid=730'
-# link2 = 'https://steamcommunity.com/market/listings/730/Gamma%20Case%20Key'
-# link3 = 'https://memy.jeja.pl/'
+            if 'all' in choice.lower() or choice == '*':
+                choice = range(len(result))
+                break
+            else:
+                choice = re.findall('\d+', choice)
+                if choice != []:
+                    choice = [int(num) for num in choice]
+                    break
 
-# thisSoup = bs4.BeautifulSoup(r.text, "html.parser")  # as ebook says r.text !
+        for ch in  choice:
+            try:
+                print(result[ch][1])
+            except IndexError:
+                print('IndexError: invalid index ', ch)
+                continue
 
 # price_sell_table = thisSoup.select('#market_commodity_forsale_table')
 # table = thisSoup.find(lambda tag: tag.name=='market_commodity_forsale_table' and tag.has_attr('id') and tag['id']=="market_commodity_forsale_table")
@@ -120,4 +133,4 @@ while True:
 
     input('Loop again...')
 
-input('End of excecution...')
+print('End of excecution...')
